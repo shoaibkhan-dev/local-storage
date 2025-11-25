5466d1f0157220bb6a5fa04750ea7280f23a40d0
Shoaib khan <shoaibmangal@yahoo.com>
2025-11-25 23:47:19 +0500

    local storage practice

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const App = () => {
+    
+  // const age = localStorage.getItem('age')
+  // const user = localStorage.getItem('user')
+
+  // localStorage.setItem('user', 'shoaib')
+  // localStorage.setItem('age', '24')
+
+  // const data = {
+  //   user: 'shoaib',
+  //   age: 23,
+  //   gender: 'male'
+  // }
+  // localStorage.setItem('data', JSON.stringify(data))
+  // localStorage.getItem(JSON.parse(data))
+
+  // const data = JSON.parse(localStorage.getItem('data'))
+  // console.log(data)
+
+  localStorage.setItem('name', 'shoaib')
+  localStorage.setItem('age', 23)
+  const user = localStorage.getItem('user')
+  console.log(user)
+
+  const person = {
+    name: 'shoaib',
+    age: 23,
+    city: 'Peshawar'
+  }
+  localStorage.setItem('person', JSON.stringify(person))
+  const data = JSON.parse(localStorage.getItem('data'))
+  console.log(data)
+  
+
+
+  
+
+
+
+  
+  
+
+  return (
+    <div>
+     
+    </div>
+  )
+}
+
+export default App
