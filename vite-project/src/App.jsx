f66d4b05b0b4ebc6d706f379940bf6254f225158
Shoaib khan <shoaibmangal@yahoo.com>
2025-12-03 23:40:10 +0500

    console.log varaibles

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -62,7 +62,9 @@ let name = 'MY NAME IS SHOAIB'
 let age = 'My Age is 23'
 let learn = 'i am learning javaScript'
 
-console.log(name, age, learn)
+console.log(name)
+onsole.log(age)
+onsole.log(learn)
 
 }
 
